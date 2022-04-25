fe6da51e01106575da336d23741b7da1335d3309
Conner Fissell <connerfissell91@gmail.com>
2022-04-24 21:41:37 -0700

    got docker-compose dev and prod to work with separate prod and dev Dockerfiles. Is it worth it to containerize React apps? Gotta look into that.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{`Ohhh hey dere!! Whatcha doinnn ${process.env.REACT_APP_NAME} bla`}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Cocktail Component</h2>
     </div>
   );
 }
