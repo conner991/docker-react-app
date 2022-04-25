32a88ba27fa036d488f6a5366202227553edfafa
Conner Fissell <connerfissell91@gmail.com>
2022-04-24 20:43:20 -0700

    Setting up docker compose, and prod serve

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{`Ohhh hey dere!! Whatcha doinnn ${process.env.REACT_APP_NAME} bla`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
